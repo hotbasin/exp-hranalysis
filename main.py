5ed0ca9ace3ea56eeca28ce639827a4e36094abb
Ded Wildfield <wildfielded@wildfielded.site>
2023-10-12 15:47:03 +0800

    Workday save2 12-10-2023

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ sys.path.append('VENV\\Lib\\site-packages')
 
 from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get('/')
+async def server_root() -> str:
+    return 'It is works'
+
 #####=====----- THE END -----=====#########################################
