f7ab759173ec902061b9bbab222af6fbcc41e6ae
Stalk Spectrum <stalkspectrum@wildfielded.site>
2023-10-12 15:43:02 +0800

    Workday save1 12-10-2023

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import sys
+sys.path.append('VENV\\Lib\\site-packages')
+
+from fastapi import FastAPI
+
+#####=====----- THE END -----=====#########################################
